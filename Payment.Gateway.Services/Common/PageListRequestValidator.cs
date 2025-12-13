fb9da13f5ab3b70fb327c42c9156da6d2ab6a77b
Kishan Thummar <thummarkishan888@gmail.com>
2025-12-13 15:32:07 +0530

    miss chanegs

--- Payment.Gateway.Services/Common/PageListRequestValidator.cs
+++ Payment.Gateway.Services/Common/PageListRequestValidator.cs
@@ -1,10 +1,10 @@
-﻿using Payment.Gateway.DTOs.Transaction;
+﻿using Payment.Gateway.DTOs.Common;
 
 namespace Payment.Gateway.Services.Common
 {
     public static class PageListRequestValidator
     {
-        public static void Normalize(TransactionListRequestDto request)
+        public static void Normalize(PageRequest request)
         {
             request.Page = request.Page < 1 ? 1 : request.Page;
             request.PageSize = request.PageSize switch
